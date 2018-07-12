be61c4149b1e26d3d592bb4538e3863f52d245f5
kevinJser <442505757@qq.com>
2018-07-12 17:50:48 +0800

    去掉部分注释

--- js/popup.js
+++ js/popup.js
@@ -430,7 +430,7 @@ $(function () {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, obj, function (response) {
                 console.log("Send Success");
-                // window.close();
+                window.close();
             });            
         });
     });
